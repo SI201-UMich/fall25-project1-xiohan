ac5ebfd396eea1f26ed14cf9fb4c03385d0cf234
aka <fumf446@gmail.com>
2025-10-05 15:20:51 -0400

    csv to dictionary function

--- project1.py
+++ project1.py
@@ -1,3 +1,32 @@
 # Name: Amanda Xiaohan Huang
 # UID: 20979264
 # Email: xiohan@umich.edu
+
+
+# Planned Calculations - What is the average body mass of all female Adélie penguins in Dream island?
+#                        What is the average bill length of male penguins above the body mass of 4,200g?
+
+import csv
+
+def filedic(filename):
+    data = []
+    with open(filename) as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            data.append(row)
+    return data
+
+
+
+
+print(filedic('penguins.csv'))
+
+
+
+# def avg_mass_Fadelie_Dream(header):
+
+
+    
+
+
+# def avg_bill_Mmass4k():
