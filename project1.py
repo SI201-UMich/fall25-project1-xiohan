1f64cc3e38a4c3aea218713ce9d96682b990fac3
aka <fumf446@gmail.com>
2025-10-06 18:14:04 -0400

    made new function that uses differennt columns as the first function to replace the previous function

--- project1.py
+++ project1.py
@@ -4,7 +4,7 @@
 
 
 # Planned Calculations - What is the average body mass of all female Adélie penguins in Dream island?
-#                        What is the average bill length of male penguins above the body mass of 4,200g?
+#                        What is the average bill length of and max flipper length of penguins in 2007?
 
 import csv
 
@@ -16,31 +16,37 @@ def filedic(filename):
             data.append(row)
     return data
 
+def avg(values):
+    return sum(values)/len(values)
+
 def avg_mass_Fadelie_Dream(data): # columns: species, island, gender, mass - 4
     masses = []
     for line in data:
         if line['species'] == 'Adelie' and line['island'] == 'Dream' and line['sex'] == 'female':
             masses.append(float(line['body_mass_g']))
 
-    if len:
-        avg = sum(masses) / len(masses)
-        return f"{avg:.2f} grams"
+    if masses:
+        return f"The average mass is {avg(masses):.2f} grams"
     else:
         return None
 
 
-
-def avg_bill_Mmass4k(data, min = 4200): # columns: gender, mass, bill length - 3
-    bill_lengths = []
+def avg_bill_maxflip_2007(data): # columns: bill length, flipper length, year
+    bill = []
+    flip = []
     for line in data:
-        if line['sex'] == 'male':
-            if float(line['body_mass_g']) > min:
-                bill_lengths.append(float(line['bill_length_mm']))
-    if bill_lengths:
-        avg = sum(bill_lengths) / len(bill_lengths)
-        return f"{avg:.2f} millimeters"
+        bill_val = line['bill_length_mm']
+        flip_val = line['flipper_length_mm']
+
+        if bill_val != "NA" and flip_val != "NA":
+            bill.append(float(bill_val))
+            flip.append(int(flip_val))
+    if bill and flip:
+        return f"The average bill length is {avg(bill):.2f} millimeters, and the max flipper length is {max(flip)} millimeters."
     else:
         return None
+        
+
     
 def report(filename, avg1, avg2):
     with open(filename, "w") as file:
@@ -49,7 +55,7 @@ def report(filename, avg1, avg2):
         file.write("What is the average body mass of all female Adelie penguins in Dream island?\n")
         file.write(avg1 + "\n\n")
         
-        file.write("What is the average bill length of male penguins above 4200g?\n")
+        file.write("What is the average bill length of and max flipper length of penguins in 2007?\n")
         file.write(avg2)
 
 
@@ -57,7 +63,7 @@ def main():
     data = filedic('penguins.csv')
 
     x = avg_mass_Fadelie_Dream(data)
-    y = avg_bill_Mmass4k(data)
+    y = avg_bill_maxflip_2007(data)
 
     report("penguin_report.txt", x, y)
 
